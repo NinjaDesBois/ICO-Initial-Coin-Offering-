c13ca68ae2063ec718a473e2741a30b793031956
CryptoninjadesBois <NINJADESBOIS@protonmail.com>
2022-04-17 00:59:40 +0000

    first commit

---
+++ hardhat-folder/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xd00d369fa624c15d84d707f6c71927319d4631d2";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
